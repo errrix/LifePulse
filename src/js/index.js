bf5e8fb5f5ff34d503db53f7cd6a5d3531beaa9b
errrix <erix.gml@gmail.com>
2018-10-20 21:46:14 +0300

    header-search

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,30 @@
 import '../scss/all.scss';
+import $ from "jquery";
+
+$(".header-search").on("click", function () {
+
+    if ($(".search-block").hasClass("hide")) {
+        $(".search-block").removeClass("hide");
+        $(".search-block").addClass("show");
+        setTimeout(function () {
+            $(".search-block form").removeClass("hide");
+        }, 600);
+    } else {
+        $(".search-block form").addClass("hide");
+        setTimeout(function () {
+            $(".search-block").removeClass("show");
+            $(".search-block").addClass("hide");
+            $(".search-block.hide").css({'animation': 'search-hide .6s'});
+        }, 300)
+    }
+
+});
+
+$(".search-block .btn-close").on("click", function () {
+        $(".search-block form").addClass("hide");
+        setTimeout(function () {
+            $(".search-block").removeClass("show");
+            $(".search-block").addClass("hide");
+            $(".search-block.hide").css({'animation': 'search-hide .6s'});
+        }, 300)
+});
