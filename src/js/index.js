b66ae62b455938e8f5c6bb60c2d1ac26ea3a76a8
Evgeniy <yevhen.ivanov@ab-soft.net>
2018-10-21 15:48:16 +0300

    Add slide bg-image

--- src/js/index.js
+++ src/js/index.js
@@ -25,3 +25,28 @@ $(".search-block .btn-close").on("click", function () {
             $(".search-block.hide").css({'animation': 'search-hide .6s'});
         }, 300)
 });
+
+// Change header-bg
+$(function() {
+    setTimeout(function() {
+        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner2.jpg)'});
+    }, 3000);
+    setTimeout(function() {
+        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner3.jpg)'});
+    }, 6000);
+    setTimeout(function() {
+        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner4.jpg)'});
+    }, 9000);
+    setTimeout(function() {
+        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner5.jpg)'});
+    }, 12000);
+    setTimeout(function() {
+        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner6.jpg)'});
+    }, 15000);
+    setTimeout(function() {
+        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner7.jpg)'});
+    }, 18000);
+    setTimeout(function() {
+        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner8.jpg)'});
+    }, 21000);
+});
