3b57701f33525ed53627f37e477eb32b4d548d0c
errrix <erix.gml@gmail.com>
2018-10-23 22:49:09 +0300

    fix search

--- src/js/index.js
+++ src/js/index.js
@@ -9,26 +9,28 @@ $(".img-slider").slick({
 });
 
 $(".header-search").on("click", function () {
-
-    if ($(".search-block").hasClass("hide")) {
-        $(".search-block").removeClass("hide").addClass("show");
-        setTimeout(function () {
-            $(".search-block form").removeClass("hide");
-        }, 600);
-    } else {
-        $(".search-block form").addClass("hide");
-        setTimeout(function () {
-            $(".search-block").removeClass("show").addClass("hide");
-            $(".search-block.hide").css({'animation': 'search-hide .6s'});
-        }, 300)
-    }
+    $(".search-block").toggleClass("hide");
+    // if ($(".search-block").hasClass("hide")) {
+    //     $(".search-block").removeClass("hide").addClass("show");
+    //     setTimeout(function () {
+    //         $(".search-block form").removeClass("hide");
+    //     }, 600);
+    // } else {
+    //     $(".search-block form").addClass("hide");
+    //     setTimeout(function () {
+    //         $(".search-block").removeClass("show").addClass("hide");
+    //         $(".search-block.hide").css({'animation': 'search-hide .6s'});
+    //     }, 300)
+    // }
 
 });
 
 $(".search-block .btn-close").on("click", function () {
-        $(".search-block form").addClass("hide");
-        setTimeout(function () {
-            $(".search-block").removeClass("show").addClass("hide");
-            $(".search-block.hide").css({'animation': 'search-hide .6s'});
-        }, 300)
+    $(".search-block").toggleClass("hide");
+        // $(".search-block form").addClass("hide");
+        // setTimeout(function () {
+        //     $(".search-block").removeClass("show").addClass("hide");
+        //     $(".search-block.hide").css({'animation': 'search-hide .6s'});
+        // }, 300)
 });
+
