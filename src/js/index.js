b27506d28927e4203cdb5c8d7895be35061fa993
errrix <erix.gml@gmail.com>
2018-10-23 22:30:17 +0300

    fix shadow

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,11 @@
 import '../scss/all.scss';
 import $ from "jquery";
-import slick from "slick-carousel"
+import slick from "slick-carousel";
 
 $(".img-slider").slick({
     autoplay: true,
-    autoplaySpeed: 4000
+    autoplaySpeed: 4000,
+    arrow: false
 });
 
 $(".header-search").on("click", function () {
@@ -31,28 +32,3 @@ $(".search-block .btn-close").on("click", function () {
             $(".search-block.hide").css({'animation': 'search-hide .6s'});
         }, 300)
 });
-
-// Change header-bg
-// $(function() {
-//     setTimeout(function() {
-//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner2.jpg)'});
-//     }, 3000);
-//     setTimeout(function() {
-//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner3.jpg)'});
-//     }, 6000);
-//     setTimeout(function() {
-//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner4.jpg)'});
-//     }, 9000);
-//     setTimeout(function() {
-//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner5.jpg)'});
-//     }, 12000);
-//     setTimeout(function() {
-//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner6.jpg)'});
-//     }, 15000);
-//     setTimeout(function() {
-//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner7.jpg)'});
-//     }, 18000);
-//     setTimeout(function() {
-//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner8.jpg)'});
-//     }, 21000);
-// });
