cef117e40af51e1ed4cc7fb739792f4554f694a0
errrix <erix.gml@gmail.com>
2018-10-23 21:48:22 +0300

    sub-menu

--- src/js/index.js
+++ src/js/index.js
@@ -27,26 +27,26 @@ $(".search-block .btn-close").on("click", function () {
 });
 
 // Change header-bg
-$(function() {
-    setTimeout(function() {
-        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner2.jpg)'});
-    }, 3000);
-    setTimeout(function() {
-        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner3.jpg)'});
-    }, 6000);
-    setTimeout(function() {
-        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner4.jpg)'});
-    }, 9000);
-    setTimeout(function() {
-        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner5.jpg)'});
-    }, 12000);
-    setTimeout(function() {
-        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner6.jpg)'});
-    }, 15000);
-    setTimeout(function() {
-        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner7.jpg)'});
-    }, 18000);
-    setTimeout(function() {
-        $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner8.jpg)'});
-    }, 21000);
-});
+// $(function() {
+//     setTimeout(function() {
+//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner2.jpg)'});
+//     }, 3000);
+//     setTimeout(function() {
+//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner3.jpg)'});
+//     }, 6000);
+//     setTimeout(function() {
+//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner4.jpg)'});
+//     }, 9000);
+//     setTimeout(function() {
+//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner5.jpg)'});
+//     }, 12000);
+//     setTimeout(function() {
+//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner6.jpg)'});
+//     }, 15000);
+//     setTimeout(function() {
+//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner7.jpg)'});
+//     }, 18000);
+//     setTimeout(function() {
+//         $('header').css({'background-image':'linear-gradient(to left, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.4) 100%), url(../../../dist/img/header-baner8.jpg)'});
+//     }, 21000);
+// });
