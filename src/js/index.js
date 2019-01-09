c6203d5909d9d3f2b5e809012a6515d2da211085
Герман Новиков <erix.gml@gmail.com>
2019-01-09 23:38:06 +0200

    new homepage design

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,8 @@
 import '../scss/all.scss';
 import $ from "jquery";
-import slick from "slick-carousel";
 
-$(".img-slider").slick({
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-    fade: true,
-    infinite: true,
-    speed: 1200,
-    dots: false
-});
-
-$(".header-search").on("click", function () {
-    $(".search-block").toggleClass("hide");
-    // if ($(".search-block").hasClass("hide")) {
-    //     $(".search-block").removeClass("hide").addClass("show");
-    //     setTimeout(function () {
-    //         $(".search-block form").removeClass("hide");
-    //     }, 600);
-    // } else {
-    //     $(".search-block form").addClass("hide");
-    //     setTimeout(function () {
-    //         $(".search-block").removeClass("show").addClass("hide");
-    //         $(".search-block.hide").css({'animation': 'search-hide .6s'});
-    //     }, 300)
-    // }
-
-});
-
-$(".search-block .btn-close").on("click", function () {
-    $(".search-block").toggleClass("hide");
-        // $(".search-block form").addClass("hide");
-        // setTimeout(function () {
-        //     $(".search-block").removeClass("show").addClass("hide");
-        //     $(".search-block.hide").css({'animation': 'search-hide .6s'});
-        // }, 300)
-});
-
+if ($('.single-card')) {
+    $('.single-card .top-line').each(function () {
+        $(this).css('width', $(this).attr('data-money-diagram') + '%');
+    });
+}
