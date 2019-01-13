6326e607f74fdda1099916bf6856688b18897779
Герман Новиков <erix.gml@gmail.com>
2019-01-14 00:33:33 +0200

    some fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,11 @@
 import '../scss/all.scss';
 import $ from "jquery";
+import popup from './popup'
+
+popup();
 
 if ($('.money-count')) {
     $('.single-card .top-line, .shortInfoBlock .top-line').each(function () {
         $(this).css('width', $(this).attr('data-money-diagram') + '%');
     });
 }
-// TODO Немного подправил JS, так как в карточке тоже используем эту же полосу сбора средств и протестировал ТУДУ
---
+++ src/js/popup.js
@@ -0,0 +1,29 @@
+import $ from "jquery";
+
+export default function popup() {
+
+    //открываем попап логина
+    $('.header-login-popup').on('click', function () {
+        $('.popup').removeClass('hide-popup');
+    });
+
+    //закрываем попап логина
+    $('.popup').on('click', function (e) {
+       if(!$.contains(document.querySelector('.popup'), e.target)) {
+           $(this).addClass('hide-popup');
+       }
+    });
+
+    //открываем окно восстановить пароль
+    $('.popup .forgot-password').on('click', function () {
+        $('.popup-step.m--login').addClass('hide-step');
+        $('.popup-step.m--restore-password').removeClass('hide-step');
+    });
+
+    //возвращаемся с формы восстановить пароль на форму логина
+    $('.popup-step.m--restore-password .button-back').on('click', function () {
+        $('.popup-step.m--restore-password').addClass('hide-step');
+        $('.popup-step.m--login').removeClass('hide-step');
+    });
+
+}
