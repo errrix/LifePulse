b4656b297d09e01214dab9b98d1433e3f3445154
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-01-13 02:55:01 +0200

    Was end single-card page

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,9 @@
 import '../scss/all.scss';
 import $ from "jquery";
 
-if ($('.single-card')) {
-    $('.single-card .top-line').each(function () {
+if ($('.money-count')) {
+    $('.single-card .top-line, .shortInfoBlock .top-line').each(function () {
         $(this).css('width', $(this).attr('data-money-diagram') + '%');
     });
 }
+// TODO Немного подправил JS, так как в карточке тоже используем эту же полосу сбора средств и протестировал ТУДУ
