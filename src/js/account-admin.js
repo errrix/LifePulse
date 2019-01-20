4c6f49cba5da10e809a0f99a3e90c68b4a0d1252
Герман Новиков <erix.gml@gmail.com>
2019-01-20 23:05:54 +0200

    view campaign

--- src/js/account-admin.js
+++ src/js/account-admin.js
@@ -20,10 +20,26 @@ export default (function accountAdmin() {
         $(this).parents('.button-block').find('.popup.m--admin-edit-category').removeClass('hide-popup');
     });
 
-    //открыть жалобу в попапе
+    //открыть жалобу и причину бана в попапе
 
     $('.admin-open-appeal').on('click', function () {
         $(this).parents('.button-block').find('.popup.m--admin-show-appeal').removeClass('hide-popup');
+    });
+
+    //попапы модерирования заявки
+
+    $('.account-admin-campaign .button-block button').on('click', function () {
+        $('.popup.m--view-campaign .popup-step').addClass('hide-step');
+        if ($(this).hasClass('campaign-back')) {
+            $('.popup.m--view-campaign .popup-step.m--campaign-back').removeClass('hide-step');
+            $('.popup.m--view-campaign').removeClass('hide-popup');
+        } else if($(this).hasClass('campaign-publish')) {
+            $('.popup.m--view-campaign .popup-step.m--campaign-publish').removeClass('hide-step');
+            $('.popup.m--view-campaign').removeClass('hide-popup');
+        }else if($(this).hasClass('campaign-delete')) {
+            $('.popup.m--view-campaign .popup-step.m--campaign-delete').removeClass('hide-step');
+            $('.popup.m--view-campaign').removeClass('hide-popup');
+        }
     });
 
 })();
