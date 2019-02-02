ee7d647975154b729351e4796894c2b872a924f4
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-02-02 02:30:11 +0200

    End FAQ

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,14 +1,26 @@
 import $ from "jquery";
 
 export default (function faq() {
+    // $('.faq-dropdown-block .faq-dropdown-question').on('click', function () {
+    //     if($(this).parent().hasClass('show-more')) {
+    //         $(this).parent().toggleClass('show-more');
+    //         $(this).parent().find(".faq-dropdown-show-more").hide(200);
+    //     } else {
+    //         $(this).parent().toggleClass('show-more');
+    //         $(this).parent().find(".faq-dropdown-show-more").show(200);
+    //     }
+    // });
 
-    $('.faq-dropdown-block .faq-dropdown-item').on('click', function () {
-        if($(this).find('.faq-dropdown-show-more').hasClass('faq-dropdown-hide')) {
-            $(this).find('.faq-dropdown-show-more').toggleClass('faq-dropdown-hide');
-            $(this).find('.faq-dropdown-question').addClass('active');
+    $('.faq-dropdown-block .faq-dropdown-question').on('click', function () {
+        console.log($(this).next(".faq-dropdown-show-more"))
+        if($(this).parent().hasClass('show-more')) {
+            // $(this).next(".faq-dropdown-show-more").css('max-height', '0')
+            $(this).parent().toggleClass('show-more');
         } else {
-            $(this).find('.faq-dropdown-show-more').addClass('faq-dropdown-hide');
-            $(this).find('.faq-dropdown-question').removeClass('active');
+            // $(this).next(".faq-dropdown-show-more").css('max-height', '500px');
+            $(this).parent().toggleClass('show-more');
         }
     });
+
+
 })();
