c473628bb40726ae800bc48d752bcb3110c8c42b
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-02-02 03:01:47 +0200

    Add Donate Section

--- src/js/faq.js
+++ src/js/faq.js
@@ -2,7 +2,6 @@ import $ from "jquery";
 
 export default (function faq() {
     $('.faq-dropdown-block .faq-dropdown-question').on('click', function () {
-        console.log($(this).next(".faq-dropdown-show-more"))
         if($(this).parent().hasClass('show-more')) {
             $(this).parent().toggleClass('show-more');
         } else {
