616d9c30ea83e70a9347298b9640c8ce98fcc393
german.n <german.n@mserverone.com>
2019-04-10 16:47:09 +0300

    add users in adminpanel

--- src/js/pages/admin/users.js
+++ src/js/pages/admin/users.js
@@ -5,6 +5,36 @@ import AdminHeader from '../../components/adminHeader'
 
 class users extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            allUsers: []
+        };
+
+        // this.getUsers = this.getUsers.bind(this);
+    }
+
+    componentDidMount() {
+        this.getUsers();
+    }
+
+    getUsers() {
+        fetch('http://165.227.11.173:3001/api/users/', {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            method: 'GET',
+            credentials: 'include'
+        })
+            .then(function (response) {
+                return response.json()
+            }).then((json) => {
+            this.setState({allUsers: json.message});
+            console.log(this.state.allUsers)
+        })
+    }
+
     render() {
         return (
             <div>
@@ -23,37 +53,38 @@ class users extends React.Component {
 
                             <div className="account-admin-appeal-wrapper">
                                 <table className="account-admin-table">
+                                    <tbody>
                                     <tr className="title">
                                         <th>№</th>
                                         <th>Пользователь</th>
+                                        <th>email</th>
                                         <th>Дата регистрации</th>
                                         <th>Заявки</th>
                                         <th>Действия</th>
                                     </tr>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>Камишанченко Оксана</td>
-                                        <td>
-                                            27.12.19
-                                        </td>
-                                        <td>
-                                            <a href="#">2112</a>
-                                            <a href="#">12</a>
-                                            <a href="#">786</a>
-                                        </td>
-                                        <td className="button-block">
-                                            <a href="/">
-                                                <svg aria-hidden="true" data-prefix="fas" data-icon="ban"
-                                                     className="svg-inline--fa fa-ban fa-w-16"
-                                                     xmlns="http://www.w3.org/2000/svg"
-                                                     viewBox="0 0 512 512">
-                                                    <path
-                                                        d="M256 8C119.034 8 8 119.033 8 256s111.034 248 248 248 248-111.034 248-248S392.967 8 256 8zm130.108 117.892c65.448 65.448 70 165.481 20.677 235.637L150.47 105.216c70.204-49.356 170.226-44.735 235.638 20.676zM125.892 386.108c-65.448-65.448-70-165.481-20.677-235.637L361.53 406.784c-70.203 49.356-170.226 44.736-235.638-20.676z"/>
-                                                </svg>
-                                            </a>
-                                        </td>
-                                    </tr>
-
+                                    {
+                                        this.state.allUsers.map((item, index) => {
+                                            return <tr key={item._id}>
+                                                <td>{index + 1}</td>
+                                                <td>{item.last_name} {item.first_name}</td>
+                                                <td>{item.email}</td>
+                                                <td>{new Date(Date.parse(item.createdAt)).toLocaleDateString()}</td>
+                                                <td>Заявки пользователя</td>
+                                                <td className="button-block">
+                                                    <a href="/">
+                                                        <svg aria-hidden="true" data-prefix="fas" data-icon="ban"
+                                                             className="svg-inline--fa fa-ban fa-w-16"
+                                                             xmlns="http://www.w3.org/2000/svg"
+                                                             viewBox="0 0 512 512">
+                                                            <path
+                                                                d="M256 8C119.034 8 8 119.033 8 256s111.034 248 248 248 248-111.034 248-248S392.967 8 256 8zm130.108 117.892c65.448 65.448 70 165.481 20.677 235.637L150.47 105.216c70.204-49.356 170.226-44.735 235.638 20.676zM125.892 386.108c-65.448-65.448-70-165.481-20.677-235.637L361.53 406.784c-70.203 49.356-170.226 44.736-235.638-20.676z"/>
+                                                        </svg>
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        })
+                                    }
+                                    </tbody>
                                 </table>
                             </div>
 
