559f508e57762de29c65b5194b035ded16a6258c
Yevhen Ivanov <yevhen.ivanov@ab-soft.net>
2019-05-10 14:30:10 +0300

    style

--- src/js/pages/success-stories.js
+++ src/js/pages/success-stories.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Subscribeblock from "../components/subscribeBlock";
 import {Link} from "react-router-dom";
+import SingleCard from "../components/singleCard";
 
 class successStories extends React.Component {
 
@@ -13,7 +14,20 @@ class successStories extends React.Component {
             <div>
                 <div className="success-stories-block">
                     <div className="container">
+                        <div className="card-block container">
+                            <ul className="card-block-list card-block-list-flex">
+                                {/*{this.state.cards ? this.state.cards.map((item) => {*/}
+                                    {/*return <li key={item._id}>*/}
+                                        {/*<SingleCard card={item}/>*/}
+                                    {/*</li>*/}
+                                {/*}) : false*/}
+                                {/*}*/}
+                            </ul>
+                            <div className="link-wrapper">
+                                <button className="link-bottom-hover" onClick={this.handleLoadMore}>Посмотреть еще</button>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
                 <Subscribeblock/>
