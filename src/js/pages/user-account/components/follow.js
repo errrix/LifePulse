7526f6b0b2875ffbee116f2fd52c7504524ff2eb
german.n <german.n@mserverone.com>
2019-05-17 09:08:35 +0300

    some fix

--- src/js/pages/user-account/components/follow.js
+++ src/js/pages/user-account/components/follow.js
@@ -1,31 +1,87 @@
 import React from "react";
 
 import SingleCard from './../../../components/singleCard'
+import url from "../../../modules/url";
+import {connect} from "react-redux";
+import SingleUserAccountCard from "./my-campaign-list";
+import Loader from "../../../components/loader";
 
 class MyFollow extends React.Component {
 
-    render() {
+    constructor(props) {
+        super(props);
 
+        this.state = {
+            cards: [],
+            loaded: false
+        };
+
+        this.getDonateCard = this.getDonateCard.bind(this);
+    }
+
+    getDonateCard() {
+        fetch(`${url}/api/card/donate/${this.props.user_id}/short`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'GET',
+            credentials: "include"
+        })
+            .then((response) => {
+                return response.json()
+            })
+            .then((json) => {
+                console.log(json.response);
+                this.setState({
+                    cards: json.response,
+                    loaded: true
+                });
+            })
+    }
+
+    componentDidMount() {
+        this.getDonateCard();
+    }
+
+    render() {
         return (
             <div className="follow active">
                 <div className="card-block">
-                    <ul className="card-block-list card-block-list-flex">
-                        <li>
-                            {/*<SingleCard/>*/}
-                        </li>
-                    </ul>
 
-                    <div className="button-wrapper">
-                        <button type="submit" className="btn m--with-loader">
-                            <span>Посмотреть еще</span>
-                            <span className="loader"/>
-                        </button>
-                    </div>
+                    {this.state.loaded ? (
+                        this.state.cards.length > 0 ?
+                            (<ul className="card-block-list card-block-list-flex">
+                                {this.state.cards.map((item) => {
+                                    return <li key={item._id}>
+                                        <SingleCard card={item}/>
+                                    </li>
+                                })}
+                            </ul>) : (<div>
+                                Тут пока нет карточек, помогите кому-либо или оставьте комментарий под заявкой и мы покажем
+                                эти карточки здесь
+                            </div>)
+                    ) : (
+                        <Loader/>
+                    )}
+
+
+                    {/*<div className="button-wrapper">*/}
+                    {/*<button type="submit" className="btn m--with-loader">*/}
+                    {/*<span>Посмотреть еще</span>*/}
+                    {/*<span className="loader"/>*/}
+                    {/*</button>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         )
     }
 };
 
+const mapStateToProps = (store) => {
+    return {
+        user_id: store.user_id
+    }
+};
 
-export default MyFollow;
+
+export default connect(mapStateToProps)(MyFollow);
